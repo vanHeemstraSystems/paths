9136355d5823bd96d3d02ba37e3e352cb104898d
willem-vanheemstrasystems <willem@vanheemstrasystems.com>
2016-04-25 09:58:23 +0100

    Modified

--- paths.js
+++ paths.js
@@ -1,5 +1,41 @@
 /*
  * paths.js
+ */
+var PathsPath = require(__dirname+'/path.js');
+
+/**
+ * Create a new Paths that let users create sub-paths.
+ * @return {Paths}
+ */
+function Paths() { }
+
+/**
+ * Create a new PathsPath object.
+ * @return {PathsPath}
+ */
+Paths.prototype.path = function() {
+  return new PathsPath();
+}
+
+//ORIGINAL module.exports = new Paths();
+module.exports = function() { return new Paths(); }
+
+
+
+
+
+
+
+
+
+
+
+
+// REMOVE ALL BELOW
+
+
+/*
+ * paths.js
  * 
  */
 var path = require('../libraries/path');
